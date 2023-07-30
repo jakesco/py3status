c89b6e46d44d097c45d1d8e4939c2064c12586ff
Jake Scott <scottj1123@gmail.com>
2023-07-30 13:34:18 -0700

    minimal i3bar json example done

--- py3status/__main__.py
+++ py3status/__main__.py
@@ -1,5 +1,32 @@
+import sys
 import json
+import signal
+import subprocess
+from time import sleep
+
+command = "date +'%a %b %d %_I:%M %p'"
+
+def print_output(full_text: str) -> None:
+    body = [[{"full_text": full_text}]]
+    print(json.dumps(body), flush=True)
+
+def handler(signum, frame):
+    print("exiting early", file=sys.stderr)
+    print(']')
+    sys.exit(0)
+
+
 
 if __name__ == "__main__":
-    test = {"version": 1}
-    print(json.dumps(test))
+    signal.signal(signal.SIGINT, handler)
+    header = { "version": 1 }
+    print(json.dumps(header), flush=True)
+    print('[', flush=True)
+    print('[]', flush=True)
+    for x in range(5):
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        output = [{"full_text": result.stdout.strip()}]
+        print(f",{output}", flush=True)
+        sleep(1)
+    print(']')
+    sys.exit(0)
